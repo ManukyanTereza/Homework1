1aa9fbe01d2a689010e40573d9ee02ed4019dc18
ManukyanTereza <88557218+ManukyanTereza@users.noreply.github.com>
2021-08-08 09:26:11 +0400

    Add files via upload

---
+++ Prog.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Homework1
+{
+    class Program
+    {
+        static void func(string [] args)
+        {
+            Console.WriteLine("Hey people");
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
